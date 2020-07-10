b463f5f8b151fe03146ed227b79ffadb9a0c9381
zjj-123 <adongguixia123@hotmail.com>
2020-07-10 18:01:24 +0800

    出生

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const webpack = require("webpack");
+
+module.exports = {
+  publicPath: "./",
+  assetsDir: "static",
+  productionSourceMap: true,
+  configureWebpack: {
+    plugins: [new webpack.DefinePlugin({})]
+  },
+  devServer: {}
+};
